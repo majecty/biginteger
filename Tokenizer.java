7a73fe0e9ac5c758b3ffe022d4b0e531dc069e58
juhyeong <majecty@gmail.com>
2013-07-10 10:42:50 +0900

    Can parse more than one token.

--- Tokenizer.java
+++ Tokenizer.java
@@ -4,6 +4,7 @@ public class Tokenizer
 {
   public enum E_Token
   {
+    NONE,
     BIGINT,
     PLUS
   }
@@ -23,6 +24,61 @@ public class Tokenizer
     }
   }
 
+  public class Eater
+  {
+    private E_Token type;
+    private boolean isFull;
+
+    public Eater()
+    {
+      Clear();
+    }
+
+    public Token GetToken()
+    {
+      if (type == E_Token.NONE)
+      {
+        return null;
+      }
+      return new Token(type);
+    }
+
+    public void Clear()
+    {
+      type = E_Token.NONE;
+      isFull = false;
+    }
+
+    public boolean CanEat(char character)
+    {
+      return isFull == false;
+    }
+
+    public void Eat(char character)
+    {
+      switch(type)
+      {
+        case NONE:
+          if (character == '+')
+          {
+            type = E_Token.PLUS;
+            isFull = true;
+          }
+          else if (Character.isDigit(character))
+          {
+            type = E_Token.BIGINT;
+          }
+          break;
+        case PLUS:
+          // Not Implemented.
+          break;
+        case BIGINT:
+          // Not Implemented.
+          break;
+      }
+    }
+  }
+
   public Tokenizer()
   {
   }
@@ -34,17 +90,37 @@ public class Tokenizer
 
   public ArrayList<Token> Parse(String line)
   {
-    String input = RemoveWhiteSpace(line);
+    String noSpaceInput = RemoveWhiteSpace(line);
 
     ArrayList<Token> list = new ArrayList<Token>();
-    if (input.equals("+"))
+
+    Eater eater = new Eater();
+    for (int i=0; i < noSpaceInput.length(); i++)
     {
-      list.add(new Token(E_Token.PLUS));
+      if (eater.CanEat(noSpaceInput.charAt(i)) == false)
+      {
+        list.add(eater.GetToken());
+        assert eater.GetToken() != null;
+        eater.Clear();
+      }
+
+      eater.Eat(noSpaceInput.charAt(i));
     }
-    else
+
+    Token lastToken = eater.GetToken();
+    if (lastToken != null)
     {
-      list.add(new Token(E_Token.BIGINT));
+      list.add(lastToken);
     }
+
+//    if (noSpaceInput.equals("+"))
+//    {
+//      list.add(new Token(E_Token.PLUS));
+//    }
+//    else
+//    {
+//      list.add(new Token(E_Token.BIGINT));
+//    }
     return list;
   }
 }
