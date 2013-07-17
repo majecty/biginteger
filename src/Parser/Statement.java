4b829ff6e3bb066792f4935de5dd7951aa6e6de6
juhyeong park <majecty@gmail.com>
2013-07-17 13:35:57 +0900

    Reindent.

--- src/Parser/Statement.java
+++ src/Parser/Statement.java
@@ -2,28 +2,28 @@ import java.util.ArrayList;
 
 public class Statement
 {
-    private ArrayList<SymbolList> syntaxList;
+  private ArrayList<SymbolList> syntaxList;
 
-    public Statement()
-    {
-        syntaxList = new ArrayList<SymbolList>();
-    }
+  public Statement()
+  {
+    syntaxList = new ArrayList<SymbolList>();
+  }
 
-    public boolean Parse(ArrayList<Token> tokenList)
-    {
-        SymbolList firstSyntax = syntaxList.get(0);
+  public boolean Parse(ArrayList<Token> tokenList)
+  {
+    SymbolList firstSyntax = syntaxList.get(0);
 
-        return firstSyntax.Parse(tokenList);
-/*
-        for (int i=0; i<firstSyntax.GetLength(); i++)
-        {
-            SymbolBase symbol = firstSyntax.Get(i);
-        }
-*/
-    }
+    return firstSyntax.Parse(tokenList);
+    /*
+      for (int i=0; i<firstSyntax.GetLength(); i++)
+      {
+      SymbolBase symbol = firstSyntax.Get(i);
+      }
+    */
+  }
 
-    public void AddSyntax(SymbolList syntax)
-    {
-        syntaxList.add(syntax);
-    }
+  public void AddSyntax(SymbolList syntax)
+  {
+    syntaxList.add(syntax);
+  }
 }
